b4befac7006c2e6462d7ffa3902163990cf8ab4c
Maikon Frandina <mknfrandina@gmail.com>
2018-06-04 14:49:38 -0300

    Aula 47

--- meat-app-starter-master/src/app/restaurant-detail/restaurant-detail.component.ts
+++ meat-app-starter-master/src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RestaurantsService } from '../restaurants/restaurants.service'
+
+import { Restaurant } from '../restaurants/restaurant/restaurant.model'
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'mt-restaurant-detail',
   templateUrl: './restaurant-detail.component.html'
 })
 export class RestaurantDetailComponent implements OnInit {
 
-  constructor() { }
+  restaurant: Restaurant
+
+  constructor(private RestaurantsService: RestaurantsService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.RestaurantsService.restaurantById(this.route.snapshot.params['id'])
+    .subscribe(restaurant => this.restaurant = restaurant)
   }
 
 }
