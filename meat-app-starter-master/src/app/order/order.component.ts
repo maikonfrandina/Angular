742f6ba152cd9938c3c36ea7a8989f8b4cd8ad21
Maikon Frandina <mknfrandina@gmail.com>
2018-06-08 17:16:03 -0300

    Aula 66

--- meat-app-starter-master/src/app/order/order.component.ts
+++ meat-app-starter-master/src/app/order/order.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { RadioOption } from '../shared/radio/radio-option.model';
+import { OrderService } from './order.service';
+import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
 
 @Component({
   selector: 'mt-order',
@@ -13,9 +16,25 @@ export class OrderComponent implements OnInit {
     {label: 'Cartão Refeição', value: 'REF'},
   ]
 
-  constructor() { }
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+  }
+
+  cartItem(): CartItem[] {
+    return this.orderService.cartItems()
+  }
+
+  increaseQty(item: CartItem){
+    this.orderService.increaseQty(item)
+  }
+
+  decreseQty(item: CartItem){
+    this.orderService.descreseQty(item)
+  }
+
+  remove(item: CartItem){
+    this.orderService.remove(item)
   }
 
 }
