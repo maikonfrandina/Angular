f62de35cdd38b65e21c9720bb8599fc30ae8b0c4
Maikon Frandina <mknfrandina@gmail.com>
2018-07-04 15:00:32 -0300

    Aula 72

--- meat-app-starter-master/src/app/order/order.component.ts
+++ meat-app-starter-master/src/app/order/order.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormGroup, FormBuilder } from '@angular/forms'
 
 import { Router } from '@angular/router'
 
-import { RadioOption } from '../shared/radio/radio-option.model';
-import { OrderService } from './order.service';
-import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
+import { RadioOption } from '../shared/radio/radio-option.model'
+import { OrderService } from './order.service'
+import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model'
 import { Order, OrderItem} from "./order.model"
 
 @Component({
@@ -12,6 +13,8 @@ import { Order, OrderItem} from "./order.model"
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+  orderForm: FormGroup
 
   delivery: number = 8
 
@@ -22,9 +25,19 @@ export class OrderComponent implements OnInit {
   ]
 
   constructor(private orderService: OrderService,
-              private router: Router) { }
+              private router: Router,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      name: this.formBuilder.control(''),
+      email: this.formBuilder.control(''),
+      emailConfirmation: this.formBuilder.control(''),
+      address: this.formBuilder.control(''),
+      number: this.formBuilder.control(''),
+      optionalAddress: this.formBuilder.control(''),
+      paymentOption: this.formBuilder.control('')
+    })
   }
 
   itemsValue(): number {
